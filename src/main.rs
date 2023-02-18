0053b86006a6766dd42d8c616722d60e485265b4
Dathan Bennett <dathan@gmail.com>
2023-02-17 21:32:36 -0800

    Add Task model and get_all_tasks method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io;
+mod model;
+
 use clap::{Arg, Command};
 use anyhow::Result;
+use crate::model::Task;
 
 fn main() -> Result<()> {
     let gc_cmd = Command::new("gc")
@@ -43,6 +45,8 @@ fn gc() -> Result<()> {
 }
 
 fn clean_up_non_focus_tasks() -> Result<()> {
+    let tasks = get_all_tasks()?;
+    println!("{:?}", tasks);
     Ok(())
 }
 
@@ -52,4 +56,12 @@ fn assign_sort_order_where_missing() -> Result<()> {
 
 fn compact_sort_order() -> Result<()> {
     Ok(())
+}
+
+fn get_all_tasks() -> Result<Vec<Task>> {
+    let output = std::process::Command::new("task")
+        .args(["export"])
+        .output()?;
+    let command_output = String::from_utf8(output.stdout)?;
+    Ok(serde_json::from_str(command_output.as_str())?)
 }
---
+++ src/model.rs
@@ -0,0 +1,55 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Task {
+    pub description: String,
+    pub entry: String,
+    pub modified: String,
+    pub status: String,
+    pub uuid: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub urgency: Option<f64>,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub tags: Vec<String>,
+    #[serde(flatten)]
+    pub udas: Map<String, Value>
+}
+
+#[cfg(test)]
+pub mod tests {
+    use anyhow::Result;
+    use serde_json::Value;
+    use crate::model::Task;
+
+    #[test]
+    pub fn can_deserialize_basic_add() -> Result<()> {
+        // if you just run `t add foo`, it'll look like this
+        let task: Task = serde_json::from_str(r#"{"description":"foo","entry":"20230214T053646Z","modified":"20230214T053646Z","status":"pending","uuid":"e237cf7e-298f-4941-9fbc-f4df6de523c8"}"#)?;
+        assert_eq!(task.description, "foo");
+        assert_eq!(task.entry, "20230214T053646Z");
+        assert_eq!(task.modified, "20230214T053646Z");
+        assert_eq!(task.status, "pending");
+        assert_eq!(task.uuid, "e237cf7e-298f-4941-9fbc-f4df6de523c8");
+        Ok(())
+    }
+
+    #[test]
+    pub fn can_deserialize_with_tags() -> Result<()> {
+        // t add +personal foo
+        let task: Task = serde_json::from_str(r#"{"description":"foo","entry":"20230214T053852Z","modified":"20230214T053852Z","status":"pending","uuid":"07dec074-9502-4035-9750-96c25983200e","tags":["personal"]}"#)?;
+        assert_eq!(task.tags, vec!["personal"]);
+        Ok(())
+    }
+
+    #[test]
+    pub fn collects_unmapped_fields() -> Result<()> {
+        // t add url:foo foo
+        let task: Task = serde_json::from_str(r#"{"description":"foo","entry":"20230214T055231Z","modified":"20230214T055231Z","status":"pending","url":"foo","uuid":"39ecff7f-75d8-4194-9759-f7415508a203"}"#)?;
+        assert_eq!(task.udas.get("url"), Some(&Value::String("foo".to_string())));
+        println!("{:?}", task);
+        println!("{}", serde_json::to_string(&task)?);
+        Ok(())
+    }
+}
